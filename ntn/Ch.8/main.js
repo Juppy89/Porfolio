1cb5f3489b6f82202a45fef67f6aa0d226954b32
Juppy <wak15002@byui.edu>
2018-05-15 10:25:32 -0600

    s

--- ntn/Ch.8/main.js
+++ ntn/Ch.8/main.js
@@ -16,4 +16,17 @@ function search() {
         alert(`You Searched for: ${input.value}`);
         event.preventDefault();
     }
+    input.value = 'Search Here';
+
+    input.addEventListener('focus', function(){
+        if (input.value==='Search Here') {
+            input.value = ''
+        }
+    }, false);
+
+    input.addEventListener('blur', function(){
+        if(input.value === '') {
+            input.value = 'Search Here';
+        }
+    }, false);
 }
